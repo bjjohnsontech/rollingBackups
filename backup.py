ce2c70ba72b016f3b6b43cdee3bd76170913ada2
BJ <bj@cargoscience.com>
2023-04-03 09:12:21 -0400

    initial commit of backup, no deletion

---
+++ backup.py
@@ -0,0 +1,27 @@
+#! /usr/local/python
+
+from datetime import datetime
+from os import walk
+from shutil import copy
+import argparse
+parser = argparse.ArgumentParser(
+                    prog='Rolling Backup',
+                    description='Given a Source, Destination, and Qty to keep, this runs from cron to manage your backups',
+                    epilog='Live Long and Prosper --- github.com/bjjohnsontech/rollingBackups')
+parser.add_argument('source', help='The source file(s) or directory', required=True)
+parser.add_argument('-f', dest='fp', help='full path to save the backup to', required=True)
+parser.add_argument('qty', type=int, help='number of backups to keep 0 for infinite', default=0)
+parser.add_argument('-d', '--format', action='dt_format',
+                    help='specify datetime format default %Y-%m-%d-%H-%M, uses datetime formatting',
+                    default='%Y-%m-%d-%H-%M', required=False)
+args = parser.parse_args()
+
+# Create backup
+dt = datetime.now().strftime(args.dt_format)
+
+copy(args.source, args.fp + '-' + dt)
+
+# remove older backups
+if args.qty:
+    print('Keep this many')
+
