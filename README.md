aa5118f7071ad5588f8bff910a9e8a1194d611ff
BJ Johnson <iam@bjjohnson.tech>
2023-05-16 07:19:57 -0400

    arguments parser added.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rollingBackups
 Create rolling backups to use with cron
+I wanted a way to backup my minecraft worlds, and I couldn't find a way to simply create rolling backups, because honestly, I won't remember to clean up old backups.
--- backup.py
+++ backup.py
@@ -1,4 +1,3 @@
-#! /usr/local/python
 
 from datetime import datetime
 from os import walk
@@ -8,12 +7,12 @@ parser = argparse.ArgumentParser(
                     prog='Rolling Backup',
                     description='Given a Source, Destination, and Qty to keep, this runs from cron to manage your backups',
                     epilog='Live Long and Prosper --- github.com/bjjohnsontech/rollingBackups')
-parser.add_argument('source', help='The source file(s) or directory', required=True)
+parser.add_argument('source', help='The source file(s) or directory')
 parser.add_argument('-f', dest='fp', help='full path to save the backup to', required=True)
 parser.add_argument('qty', type=int, help='number of backups to keep 0 for infinite', default=0)
-parser.add_argument('-d', '--format', action='dt_format',
+parser.add_argument('-d', '--format',
                     help='specify datetime format default %Y-%m-%d-%H-%M, uses datetime formatting',
-                    default='%Y-%m-%d-%H-%M', required=False)
+                    default='%Y-%m-%d-%H-%M', dest='dt_format', required=False)
 args = parser.parse_args()
 
 # Create backup
